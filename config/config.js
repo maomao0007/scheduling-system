4540bb9c6b61784fbf689d2125d1f2fd2e397e5c
maomao0007 <skylifepic07@gmail.com>
2025-03-01 13:44:37 +0800

    fix: environment variable loading

--- config/config.js
+++ config/config.js
@@ -1,18 +1,16 @@
-require('dotenv').config();
-
 module.exports = {
-  'development': {
-    'use_env_variable': 'DATABASE_URL',
-    'dialect': 'mysql'
+  development: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'mysql',
   },
-  'test': {
-    'username': 'root',
-    'password': null,
-    'database': 'database_test',
-    'host': '127.0.0.1',
-    'dialect': 'mysql'
+  test: {
+    username: 'root',
+    password: null,
+    database: 'database_test',
+    host: '127.0.0.1',
+    dialect: 'mysql',
   },
-  'production': {
-    'use_env_variable': 'DATABASE_URL'
+  production: {
+    use_env_variable: 'DATABASE_URL',
   }
 };
